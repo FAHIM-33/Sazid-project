3a3862a53158ccd41878a3ecdb8bb8ed3d724789
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-09-13 05:03:00 +0600

    ALL DONE

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      let peraElement = document.querySelector('.pera');
+      peraElement.classList.add('transitioned');
+    }, 1000);
+  });
+  
